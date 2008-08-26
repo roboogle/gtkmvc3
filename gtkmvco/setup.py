a08289f553a4bdd23b86598ac4422d5066519b95
Roberto Cavada <roboogle@gmail.com>
2008-08-26 09:56:46 +0000

    CLEANUP - fixed email - fixed version - removed pyc files [RC]

--- gtkmvco/setup.py
+++ gtkmvco/setup.py
@@ -8,11 +8,11 @@
 from distutils.core import setup
 
 setup(name="python-gtkmvc",
-      version="1.2.1",
+      version="1.2.2",
       description="Model-View-Controller and Observer patterns "\
                   "for PyGTK2",
       author="Roberto Cavada",
-      author_email="cavada@irst.itc.it",
+      author_email="cavada@fbk.eu",
       license="LGPL",
       url="http://pygtkmvc.sourceforge.net/",
       packages=['gtkmvc', 'gtkmvc.support', 'gtkmvc.adapters', 'gtkmvc.progen'],

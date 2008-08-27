c3f9e5717129469d9b457d2f752371e21ff5abe8
Roberto Cavada <roboogle@gmail.com>
2008-08-27 07:02:34 +0000

    BUG FIX Fixed version constant to 1.2.2 Thanks to [JB]

--- gtkmvco/gtkmvc/__init__.py
+++ gtkmvco/gtkmvc/__init__.py
@@ -24,7 +24,7 @@
 
 __all__ = ["model", "view", "controller", "observable", "observer", "support"]
 
-__version = (1,2,1)
+__version = (1,2,2)
 
 from model import Model, TreeStoreModel, ListStoreModel, TextBufferModel
 from model_mt import ModelMT

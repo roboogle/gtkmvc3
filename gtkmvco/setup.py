64eb11cf457da87e07ed72fb29b57192f4e6ddc8
Roberto Cavada <roboogle@gmail.com>
2009-02-12 09:10:13 +0000

    VERSION Changed version number to (still temporary) 1.2.3

--- gtkmvco/setup.py
+++ gtkmvco/setup.py
@@ -8,7 +8,7 @@
 from distutils.core import setup
 
 setup(name="python-gtkmvc",
-      version="1.2.2",
+      version="1.2.3",
       description="Model-View-Controller and Observer patterns "\
                   "for PyGTK2",
       author="Roberto Cavada",

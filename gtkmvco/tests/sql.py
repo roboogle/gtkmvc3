436d4ee6f1acff6571a397a260c7015709f7907e
Roberto Cavada <roboogle@gmail.com>
2009-03-22 20:54:32 +0000

    NEW TEST for testing sqlobject. At the moment it does not work, and the feature needs some reworking [RC]

--- gtkmvco/tests/sql.py
+++ gtkmvco/tests/sql.py
@@ -1,22 +1,40 @@
 import _importer
-from gtkmvc import Model
-import new
+
 from sqlobject import *
-
-cl = (Model.__metaclass__, SQLObject.__metaclass__)
-meta = new.classobj('meta', cl, {'__module__': '__main__', '__doc__': None})
-
 sqlhub.processConnection = connectionForURI('sqlite:/:memory:')
 
-class MetaPerson(SQLObject, Model):
-  __metaclass__ = meta
-  pass
-  
-class Person(MetaPerson):
+from gtkmvc.model import SQLObjectModel
+from gtkmvc import Observer
+
+
+class Person (SQLObjectModel):
   fname = StringCol()
   mi = StringCol(length=1, default=None)
   lname = StringCol()
 
+  __observable__ = ['?name']    
+  pass
+
+
+class PersonObs (Observer):
+    
+  def property_fname_value_change(self, model, old, new):
+      print "fname CHANGED!", old, new
+      return
+
+  def property_lname_value_change(self, model, old, new):
+      print "lname CHANGED!", old, new
+      return
+  pass
+
 Person.createTable()
+
 p = Person(fname="John", lname="Doe")
 print p
+
+c = PersonObs(p)
+
+p.fname = "Ciccio"
+print p
+
+

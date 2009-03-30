db4baf778449248a9dbf34724df4dced1290985b
Roberto Cavada <roboogle@gmail.com>
2009-03-30 16:42:56 +0000

    DOCS, SETUP Version is now automatically extracted from the only one source, gtkmvc.get_version()

--- gtkmvco/setup.py
+++ gtkmvco/setup.py
@@ -6,11 +6,12 @@
 # ----------------------------------------------------------------------
 
 from distutils.core import setup
+from gtkmvc import get_version
 
 setup(name="python-gtkmvc",
-      version="1.2.99",
+      version=".".join(map(str, get_version())), 
       description="Model-View-Controller and Observer patterns "\
-                  "for PyGTK2",
+                  "for developing pygtk-based applications",
       author="Roberto Cavada",
       author_email="cavada@fbk.eu",
       license="LGPL",
@@ -18,4 +19,19 @@ setup(name="python-gtkmvc",
       packages=['gtkmvc', 'gtkmvc.support', 'gtkmvc.adapters', 'gtkmvc.progen'],
       package_data={'gtkmvc.progen': ['progen.glade']},
       scripts=['scripts/gtkmvc-progen'],
+
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Environment :: X11 Applications :: GTK Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: User Interfaces',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
+          ],
+      
      )

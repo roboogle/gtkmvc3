cb020cebafff2f270aca8cefadaa3a6aa7beacb0
Roberto Cavada <roboogle@gmail.com>
2009-03-31 15:52:43 +0000

    DOCS User manual, tutorial and qsg [RC]

---
+++ trac/htdocs/docs/userman/pygtkmvc-js.js
@@ -0,0 +1,6 @@
+tex4ht=new Array() ; //2009-03-31 17:46:00
+tex4ht.fn1x0="<div class=\"js\"><span class=\"footnote-mark\"><a id=\"fn1x0\"> <sup class=\"textsuperscript\">1</sup></a></span> <a href=\"http://en.wikipedia.org/wiki/Architectural_pattern_\\%28computer_science_\\%29\" class=\"url\" ><span class=\"cmtt-8\">http://en.wikipedia.org/wiki/Architectural_pattern_\\%28computer_science_\\%29</span></a></div>";
+tex4ht.fn2x0="<div class=\"js\"><span class=\"footnote-mark\"><a id=\"fn2x0\"> <sup class=\"textsuperscript\">2</sup></a></span><span class=\"cmr-8\">For example, see </span><a href=\"http://www.object-arts.com/EducationCentre/Overviews/MVC.htm\" class=\"url\" ><span class=\"cmtt-8\">http://www.object-arts.com/EducationCentre/Overviews/MVC.htm</span></a></div>";
+ ol_hauto = 1; 
+
+

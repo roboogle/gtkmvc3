7ba4f36b12f189670e89a741869bd5b5a056b307
Roberto Cavada <roboogle@gmail.com>
2009-10-20 15:08:42 +0000

    NEW TEST Added test from a discussion made on the mailing list.

---
+++ gtkmvco/tests/test_prop_container.py
@@ -0,0 +1,38 @@
+import _importer
+from gtkmvc import Model, Observer
+
+class MyModel (Model):
+  mylist = None
+  
+  __observables__ = ("my*",)
+
+  def __init__(self):
+    Model.__init__(self)
+    self.mylist = []
+    return
+
+  pass # end of class
+
+
+class MyObserver (Observer):
+      
+  def property_mylist_before_change(self, model, prop_instance, 
+                                    meth_name, args, kwargs):
+    print "property_mylist_before_change:"
+    print model, prop_instance, meth_name, args, kwargs
+    return
+
+  pass # end of class
+
+
+o = MyObserver()
+m1 = MyModel()
+m2 = MyModel()
+for m in (m1, m2): o.observe_model(m)
+
+m1.mylist.append(10)
+m2.mylist.append(20)
+
+    
+
+

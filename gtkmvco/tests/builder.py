f44375153bbc841510662d8b398a065b7943dfb8
Tobias Weber <towb@celvina.de>
2011-03-05 14:09:12 +0000

    TEST confirm that no matter the arguments, all but the first builder.connect_signals is ignored.
    This confuses all.py

--- gtkmvco/tests/builder.py
+++ gtkmvco/tests/builder.py
@@ -42,5 +42,44 @@ class BuilderTest(unittest.TestCase):
         # Would have been nice...
         self.assertEqual({5: 1, 6:1}, self.c.events)
 
+    def testTwoInstances(self):
+        self.d = Controller()
+        self.x.connect_signals(self.c, 'a')
+        # Does nothing.
+        self.x.connect_signals(self.d, 'b')
+        self.b.clicked()
+        self.assertEqual({'a': 1}, self.c.events)
+        # Fails.
+        self.assertEqual({'b': 1}, self.d.events)
+
+    def testTwoDictionaries(self):
+        self.d = Controller()
+        self.x.connect_signals({'on_button7_clicked': self.c.on_button7_clicked}, 1)
+        # Does nothing.
+        self.x.connect_signals({'on_button7_clicked': self.d.on_button7_clicked}, 2)
+        self.b.clicked()
+        self.assertEqual({1: 1}, self.c.events)
+        # Fails.
+        self.assertEqual({2: 1}, self.d.events)
+
+class MultipleCalls(unittest.TestCase):
+    def setUp(self):
+        self.x = gtk.Builder()
+        self.x.add_from_file('test_builder.glade')
+        self.c = Controller()
+
+    def testDictionary(self):
+        self.x.connect_signals({'on_button1_clicked': self.c.on_button7_clicked}, 1)
+        self.x.get_object('button1').clicked()
+        self.assertEqual({1: 1}, self.c.events)
+
+    def testSecondCall(self):
+        self.testDictionary()
+        # Does nothing.
+        self.x.connect_signals({'on_button2_clicked': self.c.on_button7_clicked}, 2)
+        self.x.get_object('button2').clicked()
+        # Fails.
+        self.assertEqual({2: 1}, self.c.events)
+
 if __name__ == "__main__":
     unittest.main()

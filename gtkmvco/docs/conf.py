5b5b1fee1ec9bcbc93f8681d1ad972887f12694c
Roberto Cavada <roboogle@gmail.com>
2011-03-21 10:39:04 +0000

    BUG FIX Fixed the inclusion path to correctly point to ".."

--- gtkmvco/docs/conf.py
+++ gtkmvco/docs/conf.py
@@ -17,7 +17,7 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-top_dir = os.path.dirname(os.path.abspath(".."))
+top_dir = os.path.dirname(os.path.abspath("."))
 if os.path.exists(os.path.join(top_dir, "gtkmvc")):
     sys.path.insert(0, top_dir)
     pass

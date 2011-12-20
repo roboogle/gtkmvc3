bc572c5565d2e49e7b335fddee24ff8d1993242d
Tobias Weber <towb@celvina.de>
2011-12-20 20:19:19 +0000

    TESTS automate calendar

--- gtkmvco/tests/calendar.py
+++ gtkmvco/tests/calendar.py
@@ -1,51 +1,53 @@
-"""
-Like adapter10.py with an additional button. Pressing it should increment the
-date by one day.
-"""
-import _importer
-from gtkmvc import Model, Controller, View
+import datetime
+import unittest
+
 import gtk
 
-class MyView (View):
-    glade = "calendar.glade"
-    pass
+from _importer import refresh_gui
 
-import datetime
-class MyModel (Model):
-    data = datetime.datetime.today()
-    __observables__ = ('data',)
+import gtkmvc
 
-    def increment(self): self.data += datetime.timedelta(1)
-    pass # end of class
+class Model(gtkmvc.Model):
+    date = datetime.date(2011, 12, 24)
+    time = datetime.datetime(2011, 12, 24, 18)
+    __observables__ = ('date', 'time')
 
+class Date(unittest.TestCase):
+    def setUp(self):
+        self.m = Model()
+        self.v = v = gtkmvc.View()
+        v['window'] = w = gtk.Window()
+        v['calendar'] = c = gtk.Calendar()
+        w.add(c)
+        w.show_all()
+        self.c = gtkmvc.Controller(self.m, self.v)
+        refresh_gui()
+        self.adapt()
+        refresh_gui()
 
-class MyCtrl (Controller):
+    def testAdaption(self):
+        # Month is zero-based
+        self.assertEqual((2011, 11, 24), self.v['calendar'].get_date())
 
-    def register_view(self, view):
-        view['window1'].connect('delete-event', gtk.main_quit)
-        return
-    
-    def register_adapters(self):
-        self.adapt("data", "calendar")
-        return
+    def testAssign(self):
+        self.m.date = datetime.date(2010, 1, 1)
+        self.m.time = datetime.datetime(2010, 1, 1, 1)
+        self.assertEqual((2010, 0, 1), self.v['calendar'].get_date())
 
-    def on_button_print_clicked(self, button):
-        print self.model.data
-        return
+    def testDay(self):
+        self.assertEqual(self.m.date, self.m.time.date())
+        self.v['calendar'].select_day(25)
+        # Depending on subclass one of them is adapted
+        self.assertNotEqual(self.m.date, self.m.time.date())
+        # Changing date should keep time
+        self.assertEqual(18, self.m.time.hour)
 
-    def on_button_inc_clicked(self, button):
-        self.model.increment() 
-        return
-        
-    pass # end of class
+    def adapt(self):
+        self.c.adapt('date', 'calendar')
 
-# ----------------------------------------------------------------------
+class Time(Date):
+    def adapt(self):
+        self.c.adapt('time', 'calendar')        
 
-m = MyModel()
-v = MyView()
-c = MyCtrl(m, v)
-
-gtk.main()
-
-
-
+if __name__ == "__main__":
+    unittest.main()

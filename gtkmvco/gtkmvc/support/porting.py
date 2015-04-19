78748f609efaf1d11e073e2ddee7a39fbefac5be
Roberto Cavada <roboogle@gmail.com>
2015-04-19 23:14:24 +0200

    Added copyright info

--- gtkmvco/gtkmvc/support/porting.py
+++ gtkmvco/gtkmvc/support/porting.py
@@ -1,8 +1,31 @@
+#  Author: Roberto Cavada <roboogle@gmail.com>
+#
+#  Copyright (c) 2015 by Roberto Cavada
+#  Copyright (c) 2015 by Martijn Pieters
+#
+#  pygtkmvc is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+#
+#  pygtkmvc is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110, USA.
+#
+#  For more information on pygtkmvc see <http://pygtkmvc.sourceforge.net>
+#  or email to the author Roberto Cavada <roboogle@gmail.com>.
+#  Please report bugs to <roboogle@gmail.com>.
 
 
 
 # This was made by Martijn Pieters
-# See http://www.zopatista.com/python/2014/03/14/cross-python-metaclasses/
+# See http://www.zo]patista.com/python/2014/03/14/cross-python-metaclasses/
 def with_metaclass(mcls):
     def decorator(cls):
         body = vars(cls).copy()
